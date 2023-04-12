64e91dbef7cb46b64a283a64f84a79772b406112
Madushan97 <madushannipuna1997@gmail.com>
2023-04-12 06:23:47 +0530

    SwaggerConfig and seller save function

--- src/main/java/com/madushan/Web/Based/Gem/Auction/Controller/SellerController.java
+++ src/main/java/com/madushan/Web/Based/Gem/Auction/Controller/SellerController.java
@@ -1,11 +1,25 @@
 package com.madushan.Web.Based.Gem.Auction.Controller;
 
+import com.madushan.Web.Based.Gem.Auction.DTO.SellerDTO;
+import com.madushan.Web.Based.Gem.Auction.Service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/v1/seller")
+@RequestMapping(path = "api/v1/seller")
 public class SellerController {
 
-    public 
+    @Autowired
+    private SellerService sellerService;
+
+    @PostMapping
+    public String saveSeller(@RequestBody SellerDTO sellerDTO) {
+
+        sellerService.saveSeller(sellerDTO);
+        return "Done saving";
+    }
+
 }
---
+++ src/main/java/com/madushan/Web/Based/Gem/Auction/Repository/SellerRepository.java
@@ -0,0 +1,8 @@
+package com.madushan.Web.Based.Gem.Auction.Repository;
+
+import com.madushan.Web.Based.Gem.Auction.Entity.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller, Integer> {
+
+}
