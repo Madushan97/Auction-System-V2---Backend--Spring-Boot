b806cfe9d0bd2a9b6eae456fe576c779a29f5e35
Madushan97 <madushannipuna1997@gmail.com>
2023-05-27 08:01:10 +0530

    navbar changed depends  on the role

---
+++ auctionclient/src/Components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import RegisterPage from '../Seller/RegisterPage';
+import SellerList from '../Seller/SellerList';
+
+const HomePage = () => {
+  return (
+    <div>
+      <RegisterPage />
+      <SellerList />
+    </div>
+  );
+};
+
+export default HomePage;
