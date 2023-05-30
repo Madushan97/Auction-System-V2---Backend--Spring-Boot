dcb3513c94d9ce479825436f2a4212a65ee66d85
Madushan97 <madushannipuna1997@gmail.com>
2023-05-30 18:09:02 +0530

    delete,getById user endpoints

--- src/main/java/com/madushan/Web/Based/Gem/Auction/Controller/UserController.java
+++ src/main/java/com/madushan/Web/Based/Gem/Auction/Controller/UserController.java
@@ -16,25 +16,47 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+//    save user
     @PostMapping("/saveuser")
-    public String saveSeller(@RequestBody UserDTO sellerDTO) {
+    public String saveUser(@RequestBody UserDTO sellerDTO) {
 
         userService.saveUser(sellerDTO);
         return "Done saving";
     }
 
+//    get all users
     @GetMapping("/getallusers")
-    public List<UserDTO> getSellers() {
+    public List<UserDTO> getUsers() {
 
         List<UserDTO> allUsers = userService.getUsers();
         return allUsers;
     }
 
+//    update user
     @PutMapping("/update")
     public UserDTO updateUser(@RequestBody UpdateUserDTO updateUserDTO) {
 
         UserDTO userDTO = userService.updateUser(updateUserDTO);
         return userDTO;
+    }
+
+//    get by ID
+    @GetMapping(value = "/getById/{id}")
+    public UserDTO userGetById(@PathVariable(value = "id") int userId) {
+
+        return userService.getUserById(userId);
+
+    }
+
+//    delete user
+    @DeleteMapping(
+            path = "/delete",
+            params = {"id"}
+    )
+    public String deleteUser(@RequestParam(value = "id") int userId) {
+
+        userService.removeUser(userId);
+        return "User :"+ userId +" delete successfully";
     }
 
 }
