a009610c08a6824dd4f44ecd46d8a30246d6fa21
Madushan97 <madushannipuna1997@gmail.com>
2023-06-13 07:19:23 +0530

    Finished implementing 'gem get by id' endpoint functionality

--- src/main/java/com/madushan/Web/Based/Gem/Auction/Controller/GemController.java
+++ src/main/java/com/madushan/Web/Based/Gem/Auction/Controller/GemController.java
@@ -2,7 +2,10 @@ package com.madushan.Web.Based.Gem.Auction.Controller;
 
 import com.madushan.Web.Based.Gem.Auction.DTO.GemDTO;
 import com.madushan.Web.Based.Gem.Auction.Service.GemService;
+import com.madushan.Web.Based.Gem.Auction.Util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,18 +18,40 @@ public class GemController {
     @Autowired
     private GemService gemService;
 
+//    get all gems
     @GetMapping(path = "/allgems")
-    public List<GemDTO> getAllGems() {
+    public ResponseEntity<StandardResponse> getAllGems() {
 
         List<GemDTO> gemDTOList = gemService.getAllGems();
-        return gemDTOList;
+
+        return new ResponseEntity<StandardResponse>(
+                new StandardResponse(200, "All gems are Fetching successfully", gemDTOList), HttpStatus.OK
+        );
     }
 
-//    TODO
 //    gem get by ID
+    @GetMapping(
+            path = "/gem-get-by-id",
+            params = {"id"}
+    )
+    public ResponseEntity<StandardResponse> gemGetById(@RequestParam(value = "id") int gemId) {
+
+         GemDTO gemDTO = gemService.gemGetById(gemId);
+         return new ResponseEntity<StandardResponse> (
+                 new StandardResponse(200, "Gem Id :" + gemId + " Details Getting successfully", gemDTO), HttpStatus.OK
+                 );
+    }
+
+
 //    update gem
+
+
+
 //    remove gem
-//    save
+
+
+
+//    save gem
 
 
 }
